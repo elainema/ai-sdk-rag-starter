1794397b3e1c7cf8f9c9b3471560f11a2fa321a4
elaine.ma <elaine.ma@aspendigital.co>
2025-03-31 10:13:08 +0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+// NanoID 是一个轻量级、安全且高效的唯一 ID 生成器，主要用于生成短小、紧凑且唯一的字符串 ID
 import { customAlphabet } from "nanoid";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
